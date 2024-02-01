4cff75a56d285ce974ad447d560165ad13b5c90d
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-02-01 13:44:58 +0700

    📦 Extending the math type from macroquad and num_complex

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,3 +1,4 @@
+use macroquad::math::{vec2, Vec2};
 use num_complex::Complex;
 use num_traits::{Float, MulAdd};
 
@@ -35,5 +36,85 @@ impl ExtendedComplexNumber for Complex<f32> {
         let im = (1.0 - t).mul_add(self.im, t * other.im);
 
         Self::new(re, im)
+    }
+}
+
+trait NormalizationVector2 {
+    /// Normalize the value against Vec2
+    #[must_use]
+    fn normalize_from_vec2(&self, target: Vec2) -> Vec2;
+
+    /// Resize the value based on the target_vec using normalized value
+    #[must_use]
+    fn resize_vec2(&self, target_normalize: Vec2, target_size: Vec2) -> Vec2;
+}
+
+trait NormalizationComplexf32 {
+    /// Normalize the value against Vec2
+    fn normalize_from_compx(&self, target: Complex<f32>) -> Complex<f32>;
+    fn resize_compx(
+        &self,
+        target_normalize: Complex<f32>,
+        target_size: Complex<f32>,
+    ) -> Complex<f32>;
+}
+
+impl NormalizationVector2 for Vec2 {
+    fn normalize_from_vec2(&self, target: Vec2) -> Vec2 {
+        vec2(self.x / target.x, self.y / target.y)
+    }
+
+    fn resize_vec2(&self, target_normalize: Vec2, target_size: Vec2) -> Vec2 {
+        let normalized = self.normalize_from_vec2(target_normalize);
+        let aspect_ratio = normalized.x / normalized.y;
+
+        vec2(target_size.x, target_size.y / aspect_ratio)
+    }
+}
+
+impl NormalizationVector2 for Complex<f32> {
+    fn normalize_from_vec2(&self, target: Vec2) -> Vec2 {
+        vec2(self.re / target.x, self.im / target.y)
+    }
+
+    fn resize_vec2(&self, target_normalize: Vec2, target_size: Vec2) -> Vec2 {
+        let normalized = self.normalize_from_vec2(target_normalize);
+        let aspect_ratio = normalized.x / normalized.y;
+
+        vec2(target_size.x, target_size.y / aspect_ratio)
+    }
+}
+
+impl NormalizationComplexf32 for Vec2 {
+    fn normalize_from_compx(&self, target: Complex<f32>) -> Complex<f32> {
+        Complex::new(self.x / target.re, self.y / target.im)
+    }
+
+    fn resize_compx(
+        &self,
+        target_normalize: Complex<f32>,
+        target_size: Complex<f32>,
+    ) -> Complex<f32> {
+        let normalized = self.normalize_from_compx(target_normalize);
+        let aspect_ratio = normalized.re / normalized.im;
+
+        Complex::new(target_size.re, target_size.im / aspect_ratio)
+    }
+}
+
+impl NormalizationComplexf32 for Complex<f32> {
+    fn normalize_from_compx(&self, target: Complex<f32>) -> Complex<f32> {
+        Complex::new(self.re / target.re, self.im / target.im)
+    }
+
+    fn resize_compx(
+        &self,
+        target_normalize: Complex<f32>,
+        target_size: Complex<f32>,
+    ) -> Complex<f32> {
+        let normalized = self.normalize_from_compx(target_normalize);
+        let aspect_ratio = normalized.re / normalized.im;
+
+        Complex::new(target_size.re, target_size.im / aspect_ratio)
     }
 }
