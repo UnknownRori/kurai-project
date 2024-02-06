2d5f902b675835ebfb3a378f1180ad08cf11a423
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-02-07 04:23:07 +0700

    📦 Add some dummy stuff for checking it work

--- src/app.rs
+++ src/app.rs
@@ -33,6 +33,8 @@ impl App {
                 ("remilia0", "./resources/textures/remilia-scarlet/1.png"),
                 ("fairy0", "./resources/textures/fairy/fairy0001.png"),
                 ("hud", "./resources/ui/hud.png"),
+                ("stage1", "./resources/background/stage-1.png"),
+                ("mask", "./resources/ui/playable-mask.png"),
             ])
             .await
             .unwrap();
@@ -80,6 +82,32 @@ impl App {
     pub async fn draw(&mut self) {
         clear_background(BLACK);
 
+        // TODO : This stupid things should live in Stage Struct
+        let offset = vec2(0.001, 0.001) * self.window.playable_window().size().clone()
+            + self.window.playable_window().get_start().clone();
+        let texture = self.assets_manager.get_texture("stage1").unwrap();
+        let mask = self.assets_manager.get_texture("mask").unwrap();
+        draw_texture_ex(
+            &mask,
+            offset.x,
+            offset.y,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(self.window.playable_window().size().clone()),
+                ..Default::default()
+            },
+        );
+        draw_texture_ex(
+            &texture,
+            offset.x,
+            offset.y,
+            // WHITE,
+            Color::new(1f32, 1f32, 1f32, 0.5),
+            DrawTextureParams {
+                dest_size: Some(self.window.playable_window().size().clone()),
+                ..Default::default()
+            },
+        );
         update_draw(&self.world, &self.controls, &self.window);
         StageUI::draw(&self.window, &self.score_data, &self.assets_manager).await;
 
@@ -87,12 +115,12 @@ impl App {
         draw_fps(&self.window, 32.0, WHITE);
         draw_version(&self.window);
 
-        draw_rectangle(
-            self.window.playable_window().get_start().x,
-            self.window.playable_window().get_start().y,
-            self.window.playable_window().size().x,
-            self.window.playable_window().size().y,
-            Color::new(255f32, 0f32, 0f32, 0.5),
-        );
+        // draw_rectangle(
+        //     self.window.playable_window().get_start().x,
+        //     self.window.playable_window().get_start().y,
+        //     self.window.playable_window().size().x,
+        //     self.window.playable_window().size().y,
+        //     Color::new(255f32, 0f32, 0f32, 0.5),
+        // );
     }
 }
