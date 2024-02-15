68d26e8ccadc23ee224f2dd6869929817c38a40a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-02-16 05:49:33 +0700

    📦 change the game name

--- src/constant.rs
+++ src/constant.rs
@@ -5,5 +5,5 @@ pub const FULLSCREEN: bool = false;
 pub const DESIRED_ASPECT_RATIO: f32 = 4.0 / 3.0;
 pub const WINDOW_RESIZEABLE: bool = true;
 
-pub const GAME_NAME: &str = "Scarlet Project";
+pub const GAME_NAME: &str = "Kurai Project";
 pub const GAME_VERSION: &str = "0.0 alpha";
