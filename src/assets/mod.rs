f553041ba5f98f72febdac517e61a8ae50688603
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-02-22 04:08:05 +0700

    📦 added bgm assets handler

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -29,6 +29,8 @@ pub struct TextureHandler(HashMap<String, Arc<Texture2D>>);
 pub struct SfxHandler(HashMap<String, Arc<Sound>>);
 #[derive(Default)]
 pub struct ShaderHandler(HashMap<String, Arc<Material>>);
+#[derive(Default)]
+pub struct BgmHandler(HashMap<String, Arc<Sound>>);
 
 impl AssetsHandler for TextureHandler {
     type Output = Arc<Texture2D>;
@@ -46,6 +48,20 @@ impl AssetsHandler for TextureHandler {
 }
 
 impl AssetsHandler for SfxHandler {
+    type Output = Arc<Sound>;
+
+    async fn register(&mut self, name: &str, path: &str) -> Result<(), color_eyre::Report> {
+        let sound = load_sound(path).await?;
+        self.0.insert(name.to_owned(), Arc::new(sound));
+        Ok(())
+    }
+
+    fn get(&self, name: &str) -> Option<Self::Output> {
+        self.0.get(name).map(|a| Arc::clone(&a))
+    }
+}
+
+impl AssetsHandler for BgmHandler {
     type Output = Arc<Sound>;
 
     async fn register(&mut self, name: &str, path: &str) -> Result<(), color_eyre::Report> {
@@ -102,4 +118,5 @@ pub struct AssetsManager {
     pub textures: TextureHandler,
     pub sfx: SfxHandler,
     pub shaders: ShaderHandler,
+    pub bgm: BgmHandler,
 }
