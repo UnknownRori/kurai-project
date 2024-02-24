765a296ddd7204769bc255c0822f1f12f634842a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-02-24 07:27:57 +0700

    🛠️ clean up

--- src/stage/mod.rs
+++ src/stage/mod.rs
@@ -1,14 +1,15 @@
-use std::sync::{Arc, RwLock};
+pub mod utils;
+
+use utils::spawner_line;
 
 use keyframe::{keyframes, AnimationSequence};
 
-use hecs::World;
 use macroquad::prelude::*;
 use num_complex::Complex;
 use num_traits::ToPrimitive;
 
 use crate::{
-    assets::{AssetsHandler, AssetsManager},
+    assets::AssetsHandler,
     components::{Hitpoint, Movement, MovementQueue, Position},
     engine::{
         spawner::{SpawnEvent, Spawner},
@@ -16,25 +17,6 @@ use crate::{
     },
     entity::{spawn_enemy, spawn_player},
 };
-
-fn spawner_line(
-    start: f64,
-    delay_between: f64,
-    total: usize,
-    spawn_event: impl FnMut(&mut World, &AssetsManager) + 'static,
-) -> Vec<SpawnEvent> {
-    let spawn_event = Arc::new(RwLock::new(spawn_event));
-    let mut temp = Vec::with_capacity(total);
-
-    for i in 0..total {
-        let spawn_clone = Arc::clone(&spawn_event);
-        temp.push(SpawnEvent::new2(
-            start + (delay_between * i.to_f64().unwrap()),
-            spawn_clone,
-        ));
-    }
-    temp
-}
 
 pub fn stage_demo() -> Stage<'static> {
     // TODO : Refactor later
